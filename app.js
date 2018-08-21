6c4d1b0916e1cc592b2a623e599343016d9d42a7
Jemaine Brown <jembrown@jemaines-mbp.attlocal.net>
2018-08-21 13:56:42 -0500

    Setup simple error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // Middlemare for logging requests
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
@@ -10,8 +11,27 @@ const orderRoutes = require('./api/routes/orders');
 //   });
 // });
 
-// The product routes do not need '/products' at the beginning of the endpoint name
+app.use(morgan('dev')); // 'dev' is for the form of the output
+
+// Each route does not need the main endpoint name at the beginning
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+
+// Handle errors
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res,next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
